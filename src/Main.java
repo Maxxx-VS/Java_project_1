74bec4b3391df4217b1210d866e6f07f8d28a149
Высочанский Максим <126272688+Maxxx-VS@users.noreply.github.com>
2024-05-22 21:40:51 +0600

    #3 определение переменных (int, byte, short, long)+(float, double)+(char, String)+(boolean)

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,41 @@
 public class Main {
 
     public static void main(String[] args) {
-        // вывод стоки в консоль
+        System.out.println("Hello World! ");
 
-        /* оарп
-        лворплв
-        рап */
+//        int age = -56;
+//
+//        System.out.println("Возраст: " + age + ".");
+//        age = 24;
+//
+//        System.out.println(age);
 
-        System.out.println("Hello\\\"\n\tWorld! ");
+        byte age = 45;
+        System.out.println(age);
+
+        short num = -31000;
+        System.out.println(num);
+
+        long nubr = 555555555;
+        System.out.println(nubr);
+
+        float num5 = 4.45676543f;
+        System.out.println(num5);
+
+        double num7 = 5.384574890334578d;
+        System.out.println(num7);
+
+        char ch = 'h';
+        System.out.println(ch);
+
+        String user_name = "Vaxxi";
+        System.out.println(user_name);
+
+        boolean isHappy = true;
+
+
+
+
 
     }
 
