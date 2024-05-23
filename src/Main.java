112ddb21d137c47d2d4cd14899c0eb9e86410cff
Высочанский Максим <126272688+Maxxx-VS@users.noreply.github.com>
2024-05-23 22:40:06 +0600

    #4 получение данных от поьзователя

--- src/Main.java
+++ src/Main.java
@@ -6,13 +6,22 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-//        // получение строки от пользователя
-//        System.out.print("Введите ваше имя: ");
-//        String username = scan.nextLine();
-//        System.out.println("Привет, " + username);
 
-        // получение числа от пользователя
-        int num1 = scan.nextInt();
+        System.out.print("Введите первое число: ");
+        float num1 = scan.nextFloat();
+
+
+        System.out.print("Введите второе число: ");
+        float num2 = scan.nextFloat();
+
+        float res1 = num1 + num2;
+        float res2 = num1 - num2;
+        float res3 = num1 * num2;
+        float res4 = num1 / num2;
+
+        System.out.println(res1 + "\n" + res2 + "\n" + res3 + "\n" + res4 + "\n");
+
+
 
 
 
