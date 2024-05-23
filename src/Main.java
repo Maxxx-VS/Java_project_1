af664009f9c21aea4999d41a6fbb98799857a4bc
Высочанский Максим <126272688+Maxxx-VS@users.noreply.github.com>
2024-05-23 22:30:00 +0600

    #4 получение данных от поьзователя

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,19 @@
+import java.util.Scanner;
 
 public class Main {
 
+
+
     public static void main(String[] args) {
-        System.out.println("Hello World! ");
+        Scanner scan = new Scanner(System.in);
+//        // получение строки от пользователя
+//        System.out.print("Введите ваше имя: ");
+//        String username = scan.nextLine();
+//        System.out.println("Привет, " + username);
 
-//        int age = -56;
-//
-//        System.out.println("Возраст: " + age + ".");
-//        age = 24;
-//
-//        System.out.println(age);
+        // получение числа от пользователя
+        int num1 = scan.nextInt();
 
-        byte age = 45;
-        System.out.println(age);
-
-        short num = -31000;
-        System.out.println(num);
-
-        long nubr = 555555555;
-        System.out.println(nubr);
-
-        float num5 = 4.45676543f;
-        System.out.println(num5);
-
-        double num7 = 5.384574890334578d;
-        System.out.println(num7);
-
-        char ch = 'h';
-        System.out.println(ch);
-
-        String user_name = "Vaxxi";
-        System.out.println(user_name);
-
-        boolean isHappy = true;
 
 
 
