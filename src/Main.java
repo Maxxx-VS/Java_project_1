78daca674ecf802daa79a56bf4e4d55f7d3be174
Высочанский Максим <126272688+Maxxx-VS@users.noreply.github.com>
2024-05-26 12:10:13 +0600

    #9 многомерные массивы данных

--- src/Main.java
+++ src/Main.java
@@ -6,39 +6,18 @@ public class Main {
 
 
     public static void main(String[] args) {
-        // массивы данных
-//        int [] nums = new int[5];
-//        nums[0] = 45;
-//        nums[1] = 46;
-//        nums[2] = 47;
-//        nums[3] = 48;
-//        nums[4] = 49;
-//
-//        System.out.println(nums[0] + nums[0]);
-//
-//
-//        float [] nums2 = new float[] {5.0f, 6.45f, 89.95f};
-//        System.out.println(nums2[2]);
-//
-//        for (int i = 0; i < nums2.length; i++) {
-//            System.out.println(nums2[i]);
-//        }
-        int [] arr = new int[4];
-        Scanner scaner = new Scanner(System.in);
+        // многомерные массивы данных
+        char [][] dvumer = new char[2][2];
+        dvumer[0][0] = 't';
+        System.out.println(dvumer[0][0]);
 
-        for (int i = 0; i < arr.length; i++) {
-            System.out.println("Введите число: ");
-            int value = scaner.nextInt();
-            arr[i] = value;
-        }
-        int min = arr[0];
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] < min) {
-                min = arr[i];
-            }
-        }
-        System.out.println(min);
-
+        byte[][] nums = new byte[][] {
+                {5, 7},
+                {7, 3},
+                {2, 0}
+        };
+        nums[1][1] = 67;
+        System.out.println(nums[1][1]);
 
 
 
