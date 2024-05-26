cca3d238df719f371d3998f8335a51354a1fb850
Высочанский Максим <126272688+Maxxx-VS@users.noreply.github.com>
2024-05-26 11:59:46 +0600

    #8 массивы данных

--- src/Main.java
+++ src/Main.java
@@ -6,29 +6,38 @@ public class Main {
 
 
     public static void main(String[] args) {
-        // циклы for, while, do-while
-//        for (float i = 100; i > 10; i-=12) {
-//            System.out.println("Цикл: " + i);
-//        }
+        // массивы данных
+//        int [] nums = new int[5];
+//        nums[0] = 45;
+//        nums[1] = 46;
+//        nums[2] = 47;
+//        nums[3] = 48;
+//        nums[4] = 49;
 //
-//        int i = 0;
-//        while (i <= 100) {
-//            System.out.println("Цикл: " + i);
-//            i++;
-//        }
+//        System.out.println(nums[0] + nums[0]);
 //
-//        int i = 1;
-//        do {
-//            System.out.println("Цикл: " + i);
-//            i ++;
-//        } while (i < 10);
+//
+//        float [] nums2 = new float[] {5.0f, 6.45f, 89.95f};
+//        System.out.println(nums2[2]);
+//
+//        for (int i = 0; i < nums2.length; i++) {
+//            System.out.println(nums2[i]);
+//        }
+        int [] arr = new int[4];
+        Scanner scaner = new Scanner(System.in);
 
-        for (int i = 5; i < 25; i+=2) {
-            if (i % 2 == 0){
-                continue;
-            }
-            System.out.println("Элемент: " + i);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("Введите число: ");
+            int value = scaner.nextInt();
+            arr[i] = value;
         }
+        int min = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        System.out.println(min);
 
 
 
