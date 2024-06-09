164cbf5a17775058b5bde5a176fcc0179e90790b
Высочанский Максим <126272688+Maxxx-VS@users.noreply.github.com>
2024-06-09 20:10:19 +0600

    #16 ООП. Конструкторы классов

--- src/Main.java
+++ src/Main.java
@@ -7,26 +7,21 @@ public class Main {
 
 
 
+
     public static void main(String[] args) {
         // ООП классы и объект
-        Transport bmw = new Transport();
-        bmw.setValues(250.5f, 2500, "White", new byte[] {0, 0, 0});
-        String res1 = bmw.getValues();
-        System.out.println(res1);
+        Transport bmw = new Transport(250.5f, 2500, "White", new byte[] {0, 0, 0});
 
-//        bmw.speed = 250.5f;
-//        bmw.weight = 2500;
-//        bmw.color = "White";
-//        bmw.coordinate = new byte[] {0, 0, 0};
 
-        Transport truck = new Transport();
-        truck.speed = 140.5f;
-        truck.weight = 5600;
-        truck.color = "Red";
-        truck.coordinate = new byte[] {100, 0, 100};
 
-        System.out.println("Truck speed: " + truck.speed);
-        System.out.println("BMW speed: " + bmw.speed);
+        Transport truck = new Transport(5600, new byte[] {100, 0, 100});
+//        truck.speed = 140.5f;
+//        truck.weight = 5600;
+//        truck.color = "Red";
+//        truck.coordinate = new byte[] {100, 0, 100};
+//
+//        System.out.println("Truck speed: " + truck.speed);
+//        System.out.println("BMW speed: " + bmw.speed);
 
 
 
